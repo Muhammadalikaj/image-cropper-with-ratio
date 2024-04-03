6fff13ac75447a6c4cb958712ecf5df24aeb6f73
MdShakir110 <shakirbaltistani786@gmail.com>
2024-04-03 05:36:41 +0500

    image-cropper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FileInput from "./components/FileInput";
+import ImageCropper from "./components/ImageCropper";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [currentPage, setCurrentPage] = useState("choose-img");
+  const [imgAfterCrop, setImgAfterCrop] = useState("");
+
+  const onImageSelected = (selectedImg) => {
+    setImage(selectedImg);
+    setCurrentPage("crop-img");
+  };
+
+  const onCropDone = (imgCroppedArea) => {
+    const canvasEle = document.createElement("canvas");
+    canvasEle.width = imgCroppedArea.width;
+    canvasEle.height = imgCroppedArea.height;
+
+    const context = canvasEle.getContext("2d");
+
+    let imageObj1 = new Image();
+    imageObj1.src = image;
+    imageObj1.onload = function () {
+      context.drawImage(
+        imageObj1,
+        imgCroppedArea.x,
+        imgCroppedArea.y,
+        imgCroppedArea.width,
+        imgCroppedArea.height,
+        0,
+        0,
+        imgCroppedArea.width,
+        imgCroppedArea.height
+      );
+
+      const dataURL = canvasEle.toDataURL("image/jpeg");
+
+      setImgAfterCrop(dataURL);
+      setCurrentPage("img-cropped");
+    };
+  };
+
+  const onCropCancel = () => {
+    setCurrentPage("choose-img");
+    setImage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {currentPage === "choose-img" ? (
+        <FileInput onImageSelected={onImageSelected} />
+      ) : currentPage === "crop-img" ? (
+        <ImageCropper
+          image={image}
+          onCropDone={onCropDone}
+          onCropCancel={onCropCancel}
+        />
+      ) : (
+        <div>
+          <div>
+            <img src={imgAfterCrop} className="cropped-img" />
+          </div>
+          <button
+            onClick={() => {
+              setCurrentPage("crop-img");
+            }}
+            className="btn"
+          >
+            Crop
+          </button>
+
+          <button
+            onClick={() => {
+              setCurrentPage("choose-img");
+              setImage("");
+            }}
+            className="btn"
+          >
+            New Image
+          </button>
+        </div>
+      )}
     </div>
   );
 }
